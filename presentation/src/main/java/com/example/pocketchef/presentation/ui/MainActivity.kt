392b1182ddc3c9858f23801967a15e135fb76bf5
DzhAkhmad <dzhangarov1995@gmail.com>
2022-04-27 22:19:32 +0300

    Creating modules

--- presentation/src/main/java/com/example/pocketchef/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/pocketchef/presentation/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pocketchef
+package com.example.pocketchef.presentation.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,7 +7,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.pocketchef.databinding.ActivityMainBinding
+import com.example.pocketchef.presentation.R
+import com.example.pocketchef.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
