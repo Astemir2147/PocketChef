2380d94ce6c8701e78ab5705b6d57431f55206f7
kambievbeslan <kambiev.b.m@gmail.com>
2022-05-05 14:31:34 +0300

    Dao в Database

--- data/src/androidTest/java/com/example/pocketchef/data/db/database/RecipesSQLiteDatabase.kt
+++ data/src/androidTest/java/com/example/pocketchef/data/db/database/RecipesSQLiteDatabase.kt
@@ -2,11 +2,16 @@ package com.example.pocketchef.data.db.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.pocketchef.data.db.dao.IngredientDao
+import com.example.pocketchef.data.db.dao.MeasureDao
+import com.example.pocketchef.data.db.dao.RecipeDao
 import com.example.pocketchef.data.db.entities.Ingredient
 import com.example.pocketchef.data.db.entities.Measure
 import com.example.pocketchef.data.db.entities.Recipe
 
 @Database(entities = [Ingredient::class, Measure::class, Recipe::class], version = 1)
 public abstract class RecipesSQLiteDatabase : RoomDatabase() {
-
+    abstract fun measureDao(): MeasureDao
+    abstract fun ingredientDao(): IngredientDao
+    abstract fun recipeDao(): RecipeDao
 }
