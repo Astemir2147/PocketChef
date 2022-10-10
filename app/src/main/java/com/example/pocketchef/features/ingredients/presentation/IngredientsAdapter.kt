70fff92be2e3998386d4db296fd76284af6a669f
kambievbeslan <kambiev.b.m@gmail.com>
2022-10-10 23:28:59 +0300

    Добавил адаптер списка ингредиентов

--- app/src/main/java/com/example/pocketchef/features/ingredients/presentation/IngredientsAdapter.kt
+++ app/src/main/java/com/example/pocketchef/features/ingredients/presentation/IngredientsAdapter.kt
@@ -7,7 +7,10 @@ import com.example.pocketchef.R
 import com.example.pocketchef.databinding.ItemIngredientsBinding
 import com.example.pocketchef.features.ingredients.presentation.models.IngredientsListItem
 
-class IngredientsAdapter(private val ingredients: MutableList<IngredientsListItem>) : RecyclerView.Adapter<IngredientsViewHolder>() {
+class IngredientsAdapter(
+    private val ingredients: MutableList<IngredientsListItem>,
+    private val onItemClicked: (ingredientId: String) -> Unit = {}
+) : RecyclerView.Adapter<IngredientsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientsViewHolder = IngredientsViewHolder(
         ItemIngredientsBinding.bind(
             LayoutInflater.from(parent.context).inflate(R.layout.item_ingredients, parent,false)
@@ -23,6 +26,13 @@ class IngredientsAdapter(private val ingredients: MutableList<IngredientsListIte
 
     fun addIngredient(newIngredient: IngredientsListItem) {
         ingredients.add(newIngredient)
+        notifyItemChanged(ingredients.size-1)
+    }
+
+    fun refreshData(newIngredients: List<IngredientsListItem>) {
+        ingredients.clear()
+        ingredients.addAll(newIngredients)
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int  = ingredients.size
--- app/src/main/java/com/example/pocketchef/features/ingredients/presentation/IngredientsFragment.kt
+++ app/src/main/java/com/example/pocketchef/features/ingredients/presentation/IngredientsFragment.kt
@@ -1,12 +1,14 @@
 package com.example.pocketchef.features.ingredients.presentation
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pocketchef.R
 import com.example.pocketchef.databinding.FragmentIngredientsBinding
 
 
@@ -14,23 +16,50 @@ class IngredientsFragment : Fragment() {
 
     private var _binding: FragmentIngredientsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: IngredientsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
+        val ingredientsViewModel =
             ViewModelProvider(this)[IngredientsViewModel::class.java]
 
         _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setDecorator()
+        setAdapter()
         return root
+    }
+
+    private fun setAdapter() {
+        adapter = IngredientsAdapter(mutableListOf()) {
+            ingredientId -> onIngredientClick(ingredientId)
+        }
+        binding.recyclerviewIngredients.adapter = adapter
+    }
+
+    private fun onIngredientClick(ingredientId: String) {
+
+    }
+
+    private fun setDecorator() {
+        binding.recyclerviewIngredients.addItemDecoration(
+            object : RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    if (parent.getChildLayoutPosition(view) == 0) {
+                        outRect.top = resources.getDimension(R.dimen.list_item_spacing).toInt()
+                    }
+                    outRect.bottom = resources.getDimension(R.dimen.list_item_spacing).toInt()
+                }
+            }
+        )
     }
 
     override fun onDestroyView() {
